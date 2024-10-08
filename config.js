951f110bb26e254e9eee7b7ba247d2102bc69ec4
Sajith Jeewantha <112331315+sajith-jeewantha@users.noreply.github.com>
2024-10-08 23:04:02 +0530

    remove links

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const ngrok = "https://together-yeti-improved.ngrok-free.app";
+const ngrok = "your_url";
 const application_name = "ChatterHub";
 export const URL = ngrok + "/" + application_name;
 export const IMAGE_URL = URL + "/AvatarImages/";
